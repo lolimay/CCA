512c4e278b7e4478dc6737b69f894bda46a3fe48
lolimay <lolimay@lolimay.cn>
2019-11-08 14:43:21 +0800

    Init the project

---
+++ src/utils/colors.ts
@@ -0,0 +1,11 @@
+export function red(s: string) {
+    return `\u001b[31m${ s }\u001b[39m`;
+}
+
+export function green(s: string) {
+    return `\u001b[32m${ s }\u001b[39m`;
+}
+
+export function yellow(s: string) {
+    return `\u001b[33m${ s }\u001b[39m`;
+}
---
+++ src/utils/index.ts
@@ -0,0 +1,9 @@
+import { green, red, yellow } from './colors';
+import { print } from './log';
+
+export {
+    print,
+    red,
+    green,
+    yellow
+};
